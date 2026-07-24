e3979dd9690219b092db9ae2d2f6a85dc89aeb67
luzhigui <1019616525@qq.com>
2026-07-24 20:59:35 +0800

    chore: 删除临时调试脚本

--- _git_push.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location $PSScriptRoot
-git rebase --abort 2>$null
-git log --oneline -3
-git status --short | Select-Object -First 5
--- _git_status.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location $PSScriptRoot
-git rebase --abort *>$null
-git log --oneline -3 > _git_output.txt 2>&1
-git status --short >> _git_output.txt 2>&1
