720fee1787b4e433635f8d45577a3678a23c6bd5
luzhigui <1019616525@qq.com>
2026-07-25 23:31:15 +0800

    feat: 拆分精英怪组件，修复问题2026年7月25日23:49

--- core/48battle-round.js
+++ core/48battle-round.js
@@ -271,6 +271,9 @@ export function* createRoundStepper(state) {
         }
         u._doubleStriked = false;
         u._stunned = false;
+        if (typeof window._emitEvent === 'function') {
+            window._emitEvent(u, 'hp-change', { hp: u.hp, maxHp: u.maxHp, alive: u.alive, atk: u.atk, def: u.def, _stunned: false });
+        }
         u._xiaoZhaoDoubleStriked = false;
         u._bloodthirstStriked = false;
         u._linkTriggered = false;
@@ -313,6 +316,9 @@ export function* createRoundStepper(state) {
         }
         u._doubleStriked = false;
         u._stunned = false;
+        if (typeof window._emitEvent === 'function') {
+            window._emitEvent(u, 'hp-change', { hp: u.hp, maxHp: u.maxHp, alive: u.alive, atk: u.atk, def: u.def, _stunned: false });
+        }
         u._xiaoZhaoDoubleStriked = false;
         u._bloodthirstStriked = false;
         u._linkTriggered = false;
